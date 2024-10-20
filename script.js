5148ed2b7bac249047eb01569ec5060e8742db71
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-20 23:59:36 +0100

    Completed Kata

--- script.js
+++ script.js
@@ -196,3 +196,40 @@ function solution(str) {
 }
 
 console.log(solution("word"));
+
+/*
+Kata 9
+Count of positives / sum of negatives
+
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+Url: https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+*/
+
+function countPositivesSumNegatives(input) {
+  positiveNum = [];
+  negativeNum = [];
+  let negativeSum = 0;
+
+  if (input.length < 1 || input === null) {
+    return [];
+  } else {
+    for (let i = 0; i < input.length; i++) {
+      input[i] > 0 ? positiveNum.push(input[i]) : negativeNum.push(input[i]);
+    }
+
+    for (let i = 0; i < negativeNum.length; i++) {
+      negativeSum = negativeSum + negativeNum[i];
+    }
+    return [positiveNum.length, negativeSum];
+  }
+}
+
+countPositivesSumNegatives([10, 6, -11]);
