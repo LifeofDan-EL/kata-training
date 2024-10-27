df67036d6bfcbbe2cb7d1b56e699176942029d12
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-27 23:30:44 +0100

    Completed kata

--- script.js
+++ script.js
@@ -476,3 +476,65 @@ function filter_list(l) {
   }
   return newChar;
 }
+
+/*
+Kata 21
+Counting sheep...
+
+Description:
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+Url: https://www.codewars.com/kata/54edbc7200b811e956000556
+*/
+
+function countSheeps(sheep) {
+  count = 0;
+  if (sheep.length <= 0) {
+    return count;
+  } else {
+    for (let i = 0; i < sheep.length; i++) {
+      if (sheep[i] === true) count++;
+    }
+    return count;
+  }
+}
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
