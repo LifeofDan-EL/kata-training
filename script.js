6db190db5cca246088d1ebc2e0f76c29990f3cd1
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-03 21:07:27 +0100

    Completed kata

--- script.js
+++ script.js
@@ -655,4 +655,34 @@ function DNAtoRNA(dna) {
   return charArray.join("");
 }
 
-console.log(DNAtoRNA("GCAT"));
+/*
+Kata 26
+Odd or Even?
+
+Description:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+
+Url: https://www.codewars.com/kata/5949481f86420f59480000e7
+*/
+
+function oddOrEven(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) sum += array[i];
+  return sum % 2 === 0 ? "even" : "odd";
+}
+
+console.log(oddOrEven([1023, 1, 2]));
