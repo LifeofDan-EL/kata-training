51db8300a88fa06c1b03e8d1394d47e57bae9f3f
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-06 21:07:48 +0100

    Completed kata

--- script.js
+++ script.js
@@ -761,4 +761,29 @@ function disemvowel(str) {
   return firstArr.join(" ");
 }
 
-console.log(disemvowel("This website is for losers LOL"));
+/*
+Kata 30
+Descending Order
+
+Description:
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+Url: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/
+*/
+
+function descendingOrder(n) {
+  return Number(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
