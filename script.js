49a29bea985e119200b923e1f9d96f25865fd4a5
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-08 20:14:11 +0100

    Completed kata

--- script.js
+++ script.js
@@ -807,4 +807,26 @@ function stringToArray(string) {
   return string.split(" ");
 }
 
-console.log(stringToArray("Robin Singh"));
+/*
+Kata 32
+String ends with?
+
+Description:
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+Url: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+*/
+
+function solution(str, ending) {
+  arrayStr = str.split("");
+  endingStr = ending.split("");
+  test = arrayStr.slice(arrayStr.length - endingStr.length).join("");
+  return test === ending ? true : false;
+}
+
+console.log(solution("abcde", "abc"));
