f66acf12384b80cca33d681d2694469fe41aff69
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-14 20:10:17 +0100

    Completed kata

--- script.js
+++ script.js
@@ -998,15 +998,41 @@ function bubblesortOnce(a) {
   let result = [...a];
   let pass;
   for (let i = 0; i < result.length - 1; i++) {
-    console.log(i, result[i], result[i + 1]);
     if (result[i] > result[i + 1]) {
       pass = result[i + 1];
       result[i + 1] = result[i];
       result[i] = pass;
-      console.log(i, result[i], result[i + 1]);
     }
   }
   return result;
 }
 
-console.log(bubblesortOnce([9, 7, 5, 3, 1, 2, 4, 6, 8]));
+/*
+Kata 40
+Square Every Digit
+
+Description:
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+
+Url: https://www.codewars.com/kata/546e2562b03326a88e000020/
+*/
+
+function squareDigits(num) {
+  return Number(
+    num
+      .toString()
+      .split("")
+      .map((a) => Number(a) * Number(a))
+      .join("")
+  );
+}
+
+console.log(squareDigits(2112));
