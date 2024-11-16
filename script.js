604beea745922cb2cf07369bc579ed4367abd97e
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-16 21:25:26 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1035,7 +1035,7 @@ function squareDigits(num) {
   );
 }
 
-/*C
+/*
 Kata 41
 Beginner Series #3 Sum of Numbers
 
@@ -1067,4 +1067,39 @@ function getSum(a, b) {
   return sum;
 }
 
-console.log(getSum(-1, 2));
+/*
+Kata 42
+Make a function that does arithmetic!
+
+Description:
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+Try to do it without using if statements!
+
+Url: https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+*/
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case "add":
+      return a + b;
+    case "subtract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+  }
+}
+
+console.log(arithmetic(8, 2, "subtract"));
