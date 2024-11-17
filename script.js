3debb35590b27287556874df89d16acd63ba311d
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-17 20:17:14 +0100

    Complete kata

--- script.js
+++ script.js
@@ -1102,4 +1102,32 @@ function arithmetic(a, b, operator) {
   }
 }
 
-console.log(arithmetic(8, 2, "subtract"));
+/*
+Kata 43
+Sort the odd
+
+Description:
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+Url: https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+*/
+
+function sortArray(array) {
+  const oddArr = array.filter((a) => a % 2 !== 0).sort((a, b) => a - b);
+
+  let k = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      array[i] = oddArr[k];
+      k++;
+    }
+  }
+  return array;
+}
+
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
