c54073f7158707525de99241d9a8d9c526bf4f2f
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-22 16:35:16 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1230,10 +1230,25 @@ var number = function (busStops) {
   return enter - alighted;
 };
 
-console.log(
-  number([
-    [10, 0],
-    [3, 5],
-    [5, 8],
-  ])
-);
+/*
+Kata 48
+Powers of 2
+
+Description:
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+Url: https://www.codewars.com/kata/57a083a57cb1f31db7000028
+*/
+
+function powersOfTwo(n) {
+  arr = [];
+  for (let i = 0; i <= n; i++) arr.push(2 ** i);
+  return arr;
+}
+console.log(powersOfTwo(0));
