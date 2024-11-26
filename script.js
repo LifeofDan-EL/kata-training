afefadf9cb7c59bce88f33917d39bcba70441da1
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-11-26 20:51:15 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1359,4 +1359,30 @@ function countBy(x, n) {
   return z;
 }
 
-console.log(countBy(2, 5));
+/*
+Kata 52
+Century From Year
+
+Description:
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+2742 --> 28
+Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here
+
+
+Url: https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+*/
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+console.log(century(1705));
