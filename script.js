f0ca9dcbd9986a73de9f65b55ab6bf271af8b50c
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-02 22:44:58 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1542,4 +1542,26 @@ function setAlarm(employed, vacation) {
   return employed && !vacation ? true : false;
 }
 
-console.log(setAlarm(true, true));
+/*
+Kata 58
+Find Multiples of a Number
+
+Description:
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+
+
+Url: https://www.codewars.com/kata/58ca658cc0d6401f2700045f
+*/
+
+function findMultiples(integer, limit) {
+  let numArr = [];
+  for (let i = 1; i <= limit; i++) {
+    if (integer * i <= limit) numArr.push(integer * i);
+  }
+  return numArr;
+}
+
+console.log(findMultiples(5, 25));
