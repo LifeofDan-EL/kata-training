d350c71b873c0c7c2527a29acb61859774a0f612
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-04 19:14:49 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1580,4 +1580,31 @@ function getAge(inputString) {
   return Number(inputString.split("")[0]);
 }
 
-console.log(getAge("4 years old"));
+/*
+Kata 60
+The museum of incredibly dull things
+
+Description:
+The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+* Input: [1,2,3,4,5], output = [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1]
+
+
+Url: https://www.codewars.com/kata/563cf89eb4747c5fb100001b/
+*/
+function removeSmallest(numbers) {
+  const smallestIndex = numbers.indexOf(Math.min(...numbers));
+  return numbers.filter((_, index) => index !== smallestIndex);
+}
+
+console.log(removeSmallest([2, 2, 1, 2, 1]));
