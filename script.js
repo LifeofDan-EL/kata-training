d3c00ee47019313f818d2bdbb9f0b49b91fbea83
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-09 21:44:02 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1668,4 +1668,32 @@ function rentalCarCost(d) {
   const initialCost = d * 40;
   return d >= 7 ? initialCost - 50 : d >= 3 ? initialCost - 20 : initialCost;
 }
-console.log(rentalCarCost(4));
+
+/*
+Kata 64
+
+Sum The Strings
+
+Description:
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+Url: https://www.codewars.com/kata/5966e33c4e686b508700002d/
+*/
+
+function sumStr(a, b) {
+  return String(Number(a) + Number(b));
+}
+console.log(sumStr("4", "5"));
