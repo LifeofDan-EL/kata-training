f44d7ebc2fbfe058a35da646ee44eb026fbe8129
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-10 22:10:10 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1696,4 +1696,22 @@ Url: https://www.codewars.com/kata/5966e33c4e686b508700002d/
 function sumStr(a, b) {
   return String(Number(a) + Number(b));
 }
-console.log(sumStr("4", "5"));
+
+/*
+Kata 65
+
+Sort Numbers
+
+Description:
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+
+Url: https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/
+*/
+function solution(nums) {
+  return nums !== null ? nums.sort((a, b) => a - b) : [];
+}
+console.log(solution([1, 2, 3, 10, 5]));
