961766043f548cf070e328492c4a563bca87bccb
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-12 21:43:30 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1729,4 +1729,23 @@ function sumTwoSmallestNumbers(numbers) {
   const test = numbers.sort((a, b) => a - b);
   return test[0] + test[1];
 }
-console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+
+/*
+Kata 67
+Will you make it?
+
+Description:
+You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not.
+
+Url: https://www.codewars.com/kata/5861d28f124b35723e00005e/
+*/
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return mpg * fuelLeft >= distanceToPump;
+};
+
+console.log(zeroFuel(50, 25, 2));
