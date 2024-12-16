fc5d91af57290388573c0908aeb476cda322d2c9
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-16 22:52:03 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1829,4 +1829,28 @@ function comp(array1, array2) {
   } else return false;
 }
 
-console.log(comp([], null));
+/*
+Kata 71
+Double Char
+
+Description:
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+
+Good Luck!
+
+Url: https://www.codewars.com/kata/56b1f01c247c01db92000076/
+*/
+
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((a) => a + a)
+    .join("");
+}
+
+console.log(doubleChar("abcd"));
