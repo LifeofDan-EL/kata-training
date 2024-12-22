1193970f8dd8a0ffb06c1dc4f41eff4de7e15f31
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-22 21:49:12 +0100

    Completed kata

--- script.js
+++ script.js
@@ -1991,4 +1991,39 @@ function hero(bullets, dragons) {
   return bullets >= dragons * 2;
 }
 
-console.log(hero(10, 5));
+/*
+Kata 77
+Count the divisors of a number
+
+Description:
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000, but fixed tests go higher.
+
+Examples (input --> output)
+4 --> 3 // we have 3 divisors - 1, 2 and 4
+5 --> 2 // we have 2 divisors - 1 and 5
+12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+Url: https://www.codewars.com/kata/542c0f198e077084c0000c2e
+*/
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  const sqrt = Math.sqrt(n);
+
+  for (let i = 1; i <= sqrt; i++) {
+    if (n % i === 0) {
+      count += 2;
+      if (i === sqrt) {
+        count--;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(getDivisorsCnt(54));
