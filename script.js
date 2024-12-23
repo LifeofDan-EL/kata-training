d3c099d1ff719261af312e7a8a6e2225930a78d0
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-12-23 22:08:55 +0100

    Completed kata

--- script.js
+++ script.js
@@ -2026,4 +2026,28 @@ function getDivisorsCnt(n) {
   return count;
 }
 
-console.log(getDivisorsCnt(54));
+console.log(getDivisorsCnt(5999));
+
+/*
+Kata 78
+Two to One
+
+Description:
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+Url: https://www.codewars.com/kata/5656b6906de340bd1b0000ac
+*/
+
+function longest(s1, s2) {
+  return [...new Set((s1 + s2).split("").sort())].join("");
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"));
